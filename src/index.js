92a43885fe52c78d7ddcffa1985506ca87678219
CatWebs <cat.webs@outlook.com>
2026-03-11 20:18:14 -0300

    agrego dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
+import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use("/", userRouter);
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,15 @@ import express from "express";
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res) => {
-  res.send("");
+  res.send("Página de inicio");
+});
+
+userRouter.get("/contact", (req, res) => {
+  res.send("Página de contacto");
+});
+
+userRouter.get("/about", (req, res) => {
+  res.send("Página de Acerca de");
 });
 
 export default userRouter;
